a64f5b16f4601a228d82da6d392a9c5d7dc14385
anescobar <anescobar@ITEM-S60230.emea.msad.sopra>
2018-01-11 08:04:33 +0100

    Ribbon

--- currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/feign/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/feign/CurrencyExchangeServiceProxy.java
@@ -1,12 +1,20 @@
 package com.in28minutes.microservices.currencyconversionservice.feign;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.in28minutes.microservices.currencyconversionservice.domain.CurrencyConversion;
 
-@FeignClient(name="currency-exchange-service",url="localhost:8000")
+
+/****if only there is a instance of currency-exchange-service put the url**/
+//@FeignClient(name="currency-exchange-service",url="localhost:8000")
+
+
+/** If thre are two or more instaces of currency-exchange is necessary to use ribbon to resolve the url and port**/
+@FeignClient(name="currency-exchange-service")
+@RibbonClient(name="currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 	
 	@GetMapping(value="/currency-exchange/from/{from}/to/{to}")
