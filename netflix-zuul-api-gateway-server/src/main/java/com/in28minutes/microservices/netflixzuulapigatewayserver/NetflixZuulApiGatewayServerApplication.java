6e9d0f280280506c387962b9ec7fe49d9bc3817c
anescobar <anescobar@ITEM-S60230.emea.msad.sopra>
2018-01-15 07:37:14 +0100

    zipkin not work

--- netflix-zuul-api-gateway-server/src/main/java/com/in28minutes/microservices/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
+++ netflix-zuul-api-gateway-server/src/main/java/com/in28minutes/microservices/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -28,9 +26,9 @@ public class NetflixZuulApiGatewayServerApplication {
 	}
 	
 	
-	/**Configuration sleuth**/
-	@Bean
-	public AlwaysSampler defaultSample() {
-		return new AlwaysSampler();
-	}
+//	/**Configuration sleuth**/
+//	@Bean
+//	public AlwaysSampler defaultSample() {
+//		return new AlwaysSampler();
+//	}
 }
